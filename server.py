4ece9d9306bce7f3265c017094bf1822c02dad1a
Nikoliadis <nnikoliadis@nyc.gr>
2025-01-11 12:09:11 +0200

    Fixed some errors with the login and the desing in the feedback and about us

--- server.py
+++ server.py
@@ -81,7 +81,6 @@ def login():
         # Check if user exists and password matches
         if user and user.check_password(password):
             login_user(user)
-            flash('Login successful!', 'success')
             return redirect(url_for('home'))  # Redirect to home or dashboard
         else:
             flash('Invalid email/username or password', 'danger')
